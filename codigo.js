9b51a58881c80d5573bc89378654d4eac157969e
unknown <athusw@yahoo.com.br>
2022-05-25 17:07:12 -0300

    Segundo

---
+++ codigo.js
@@ -0,0 +1,16 @@
+function limpar(){
+    document.getElementById('placa').value = "";
+    document.getElementById('data').value = "";
+    document.getElementById('user').value = "";
+    document.getElementById('moto').checked = false;
+    document.getElementById('carro').checked = false;
+    document.getElementById('aluno').checked = false;
+    document.getElementById('professor').checked = false;
+    document.getElementById('tae').checked = false;
+    document.getElementById('adif').checked = false;
+    document.getElementById('rest').checked = false;
+    document.getElementById('pesq').checked = false;
+    document.getElementById('estud').checked = false;
+    document.getElementById('biblio').checked = false;
+    document.getElementById('secret').checked = false;
+}
