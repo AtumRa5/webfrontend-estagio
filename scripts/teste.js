b52dd07ea5f038eb4ad864ce2d6c31fca0bcffb7
Pombo12 <alexandreomar0202@gmail.com>
2022-05-26 15:41:24 -0300

    Adicionei um teste do javascript

---
+++ scripts/teste.js
@@ -0,0 +1,6 @@
+function procurar(){
+    let pRA=document.getElementById(ra);
+    let pSenha=document.getElementById(senha);
+    let cadstro1='arqJson/teste.json';
+    let cadastro= JSON.parse(cadstro1);
+}
