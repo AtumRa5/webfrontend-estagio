2729e5a3d8cb972881458af0711cd3321c232730
Alexandre O Marinho <97129142+Aleomar15@users.noreply.github.com>
2022-05-30 17:36:37 -0300

    Tentativa de mexer no json

--- scripts/teste.js
+++ scripts/teste.js
@@ -1,6 +1,43 @@
-function procurar(){
-    let pRA=document.getElementById(ra);
-    let pSenha=document.getElementById(senha);
-    let cadstro1='arqJson/teste.json';
-    let cadastro= JSON.parse(cadstro1);
-}
+
+    const pLogin=document.querySelector("#login");
+
+pLogin.addEventListener("click",function(e){
+    e.preventDefault();
+    const pSenha=document.querySelector("#senha");
+    const pRA=document.querySelector("#ra");
+    const valueR = pRA.value, valueS = pSenha.value;
+    console.log(valueR);
+    console.log(valueS);
+})
+    function loadJSON(url, callback) { 	   
+        let xobj = new XMLHttpRequest();      
+       xobj.overrideMimeType('arqJson/teste.json'); 	
+       xobj.open('GET', url, true); 	
+       xobj.onreadystatechange = () => { 		
+       if (xobj.readyState == 4 && xobj.status == '200') 
+            callback(xobj.responseText); 	}; 
+            xobj.send(null); }
+    
+    //Utilizando o método
+    
+   /* getJSON("arqJson/teste.json", function(err, data) {
+      if (err !== null) {
+        console.log('Ocorreu um erro' + err);
+      } else {
+        //Aqui você manipula os dados e pode percorrer e jogar no HTML 
+        //da forma que achar mais adequada.
+        console.log(data);
+      }
+    });*/
+fetch("arqJson/teste.json")
+  .then(response => response.json())
+  .then(json => console.log(json));
+
+    async function printJSON() {
+        const response = await fetch("arqJson/teste.json");
+        const json = await response.json();
+        console.log(response.json());
+    }
+
+    
+   
