2d6a5a1ada24cec9c73bf095d62355aadf66ea4c
Alexandre O Marinho <97129142+Aleomar15@users.noreply.github.com>
2022-05-31 17:30:40 -0300

    upando

---
+++ scripts/cadastro.js
@@ -0,0 +1,14 @@
+const cad=document.querySelector("#cadastro");
+
+cad.addEventListener("click",function(c){
+    c.preventDefault();
+    const cRa= document.querySelector("#ra");
+    const criSenha = document.querySelector("#criarsenha");
+    const cofSenha = document.querySelector("#confirmarsenha");
+    const valuesCRA = cRa.value , valuesCRI = criSenha.value , valuesCO = cofSenha.value;
+    console.log(valuesCRA);
+    console.log(valuesCRI);
+    console.log(valuesCO);
+    
+
+})
