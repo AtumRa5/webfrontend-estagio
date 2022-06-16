f708816c30ec6c555686cb44a6ed8c24079f47dc
¨AtumRa5¨ <athusw@yahoo.com.br>
2022-06-16 16:06:39 -0300

    organizado

--- estagiosdisponiveis.php
+++ estagiosdisponiveis.php
@@ -1,118 +1,81 @@
-<!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styleempresas.css">
-    <title>Estagios Disponiveis</title>
-</head>
-<body>
-    <div class="container">
-
-        <nav class="navbar">
-            <a class="navbar-brand" href="https://portal.ifsuldeminas.edu.br/index.php">
-            <img src="assets/IFLOGO.png"  height="100px" width="100px">
-            </a>
-
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/styleempresas.css">
+        <title>Estagios Disponiveis</title>
+    </head>
     
-        <div class="navbar-nav">
-                
-                <a class="nav-link" href="index.php">INICIO</a>
-            
-        </div>
-        <div class="navbar-nav">
-           
-                <a class="nav-link" href="noticias.html">NOTICIAS</a>
-            
-        </div>
+    <body>
+        <div class="container">
+            <nav class="navbar">
+                <a class="navbar-brand" href="https://portal.ifsuldeminas.edu.br/index.php">
+                    <img src="assets/IFLOGO.png"  height="100px" width="100px">
+                </a>
+
+                <div class="navbar-nav">
+                    <a class="nav-link" href="index.php">INICIO</a>
+                </div>
+
+                <div class="navbar-nav">
+                    <a class="nav-link" href="noticias.html">NOTICIAS</a>
+                </div>
      
-      
-        <div class="navbar-nav">
-           
-                <a class="nav-link" href="curriculos.php">CURRICULOS</a>
+                <div class="navbar-nav">
+                    <a class="nav-link" href="curriculos.php">CURRICULOS</a>
+                </div>
             
-             </div>
+                <div class="navbar-nav">
+                    <a class="nav-link" href="estagiosdisponiveis.php">ESTAGIOS</a>
+                </div>
             
-           
-           
-           
-           
-            <div class="navbar-nav">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="datas.php">DATAS</a>
+                </div>
             
-                <a class="nav-link" href="estagiosdisponiveis.php">ESTAGIOS</a>
-            </div>
-            
-              <div class="navbar-nav">
-            
-                <a class="nav-link" href="datas.php">DATAS</a>
-            </div>
-            
-            
-            
-            <div class="navbar-nav">
-            
-                <a class="nav-link" href="empresas.php">EMPRESAS</a>
-            </div>      
+                <div class="navbar-nav">
+                    <a class="nav-link" href="empresas.php">EMPRESAS</a>
+                </div>      
            
-          
-            <div class="navbar-nav">
-            
-                <a class="nav-link" href="vagas.php">VAGAS</a>
+                <div class="navbar-nav">
+                    <a class="nav-link" href="vagas.php">VAGAS</a>
+                </div>
            
-        </div>
-           
-           
-           
-            <div class="navbar-nav">
-                    
+                <div class="navbar-nav">
                     <a class="nav-link" href="ajuda.php">AJUDA</a>
-                    
-            </div>
-            <button class="button1">Sair</button>
-         
-
-                
+                </div>
             
+                <button class="button1">Sair</button>
+            </nav>
 
-
-        </nav>
-
-      
-   
-     
-        <div class="tabela">
- 
+            <div class="tabela">
                 <table class="styled-table" border="2">
-                        <thead>
-                          <tr>        
-                          <th>Estagios</th>
-                         
-                         </tr>
-                 </thead>
-               
-
-
-                            <?php include_once("cadastrarestagio.php");
-                              $vetestagio = retornaEstagio();
-                              if ( $vetestagio != null) {
-                                      foreach ( $vetestagio as $estagio) {
-                                          $nome = $estagio['nome'];
-                                          $area = $estagio['area'];
-                                          $empresa = $estagio['empresa'];
-                                          $idestagio = $estagio['idestagio'];
-                              echo('<tr> <td>'.$nome.'</td>
-                               <td>'. $area.'</td>
-                              <td>'.  $empresa.'</td>
-                               <td><a href="cadastrarestagio.php?id='.$idestagio'>Alterar</a><a href="excluirEstagio.php?id='. $idestagio.' onclick="return confirm(\'Deseja excluir?\');">Excluir</a></td> </tr>');
-                                      } // fecha foreach
-                                  } // fecha if
-                                  else {
-                                      echo("<tr><td>Nenhum registro encontrado!</td></tr>");
-                                  }
-                               ?>
+                    <thead>
+                        <tr>        
+                            <th>Estagios</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                        $vetestagio = retornaEstagio();
+                        if( $vetestagio != null){
+                            foreach($vetestagio as $estagio){
+                                $nome = $estagio['nome'];
+                                $area = $estagio['area'];
+                                $empresa = $estagio['empresa'];
+                                $idestagio = $estagio['idestagio'];
+                                echo('<tr> <td>'. $nome .'</td>
+                                <td>'. $area .'</td>
+                                <td>'.  $empresa .'</td>
+                                <td><a href="cadastrarestagio.php?id='. $idestagio .'>Alterar</a><a href="excluirEstagio.php?id=' .$idestagio. ' onclick="return confirm(\'Deseja excluir?\');">Excluir</a></td> </tr>');
+                            }
+                        }else{
+                            echo("<tr><td>Nenhum registro encontrado!</td></tr>");
+                        }
+                    ?>
                    
-                        </tbody>
+                    </tbody>
                  </table>
 
                  <button class="button">Cadastrar Estagio</button>
